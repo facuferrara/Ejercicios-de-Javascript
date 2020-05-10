166bdd1b6811b43a375e468ac478c03532171885
Facundo Ferrara <facuferrarahotmail.com>
2020-05-10 17:06:38 -0300

    ejercicio 1 terminado

---
+++ ejercicio1.9/miScript.js
@@ -0,0 +1,19 @@
+var a = 1
+var b = 2
+var c = 3
+var d = a + b + c
+
+
+console.log("Ejercicio  de Variables: " +" Suma de todas las variables: "+ d + ", Al remplazar el valor de A por B queda : ",  a=b);
+
+var nombre = "Facundo"
+var edad = "22 años"
+var cumple = "30/08/1998"
+var ciudad = "Morón"
+var ocupacion = "Albañil"
+
+//1
+console.log("Mi nombre es "+ nombre+ " tengo "+edad +" cumplo años el ",cumple,"naci y vivo actualmente en " ,ciudad," y mi ocupacion es la de ",ocupacion)
+
+
+
---
+++ scriptEje2.js
@@ -0,0 +1,6 @@
+alert("Sitio muy copado, disfrutelo con cautela !")
+var user = prompt("Bienvenido, ¿Cuál es su nombre? ");
+alert("Hola "+ user)+"!";
+var edad = prompt("¿Cuál es su edad? ");
+alert("Recuerde que debe ser +18 años para acceder a este sitio")+"!";
+alert(user +" usted cumple los requisitos ya que tiene " + edad + " años, disfrute del contenido!" );
