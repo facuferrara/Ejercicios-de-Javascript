79cecdaab361ae2d2889aa2b71ef6146d21fc516
Facundo Ferrara <facuferrarahotmail.com>
2020-05-11 16:27:18 -0300

    cambios eje 1.8

---
+++ ejercicio1.8/scripteje1.js
@@ -0,0 +1,27 @@
+var $div = document.querySelector("div");
+$div.textContent 
+// $div.textContent="<p> Estoy dentro de un parrafo</p>"
+$div.innerHTML="<p> Estoy dentro de un parrafo</p>"
+
+var h1 = document.querySelector("h1");
+h1.style.color="pink";
+var $p = document.querySelector("p")
+$p.style.color="red"
+$p.style.background="blue"
+$p.style.fontSize="50px"
+$p.style.marginTop="100px"
+$p.style.width="100"
+$p.style.height="20"
+var stilo=document.querySelector("body");
+stilo.style.backgroundColor="aquamarine"
+  
+class a{
+  constructor(height, width) {
+    this.name = 'Polygon';
+    this.sq = height;
+    this.width = width;
+  }
+}
+  h1.classList;
+  h1.querySelectorAll("a").setAttribute("https://google.com")
+  classList.toggle()
---
+++ ejercicio1.8/shuffle_app.js
@@ -0,0 +1,13 @@
+var element = document.createElement(tagName, [options]);
+function addElement () { 
+  // crea un nuevo div 
+  // y añade contenido 
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode("Hola!¿Qué tal?"); 
+  newDiv.appendChild(newContent); //añade texto al div creado. 
+
+  // añade el elemento creado y su contenido al DOM 
+  var currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+}
+
