6730760b7339e4d920ea6284de662324aab9214e
Facundo Ferrara <facuferrarahotmail.com>
2020-05-11 14:40:27 -0300

    Dom

---
+++ ejercicio1.9/scriptEje3.js
@@ -0,0 +1,10 @@
+var userEdad= prompt("¿Cuál es su edad?");
+var cantDias=365 * userEdad
+alert(" Usted lleva viviendo "+cantDias+ " dias desde que nació" );
+
+var cadena = 'What\'s wrong with the pudding?", "she\'d say with a smile.' 
+
+var cadena1 = 'I\'d often think' * "to myself\", \"Well, what\'s the worst wrong that could go wrong?". "I guess" * "now we know..." 
+
+console.log(cadena)
+console.log(cadena1)
--- scriptEje3.js
+++
@@ -1,7 +0,0 @@
-var userEdad= prompt("¿Cuál es su edad?");
-var cantDias=365 * userEdad
-alert(" Usted lleva viviendo "+cantDias+ " dias desde que nació" );
-
-var cadena1 = escape("'What's wrong with the pudding?", "she'd say with a smile.'"); 
-
-var cadena1 = escape(I'd often think' * "to myself, "Well, what's the worst wrong that could go wrong?". I guess" * "now we know...")
