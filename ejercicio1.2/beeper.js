296f85fe88f87329e0b1290c2a72da5ccec58fa6
Facundo Ferrara <facuferrarahotmail.com>
2020-05-14 16:06:59 -0300

    ejercicio 1.6

--- ejercicio1.2/beeper.js
+++ ejercicio1.2/beeper.js
@@ -1,8 +1,11 @@
-var boton = document.querySelector("button")
-var parrafo = document.querySelector("p")
-//Esta vez definimos la función fuera del evento y la llamamos cambiarTexto
-var cambiarTexto = function(){
-    parrafo.textContent = "Alguien hizo click!"
-}
-// En el callback pasamos directamente el nombre de la función cambiarTexto
-boton.addEventListener("click", cambiarTexto)
+function CreaNuevoParrafo() {
+    var h = document.getElementById("myH2");
+    h.insertAdjacentHTML("afterend", "<p>BEEP</p>");
+  }
+
+  function colocarSombra() {
+    document.getElementById("Sombrear").classList.toggle("mystyle");
+  }
+  function colocarSombraVCallback(){
+    var seEjecutaEnEvento =colocarSombra();
+  }
